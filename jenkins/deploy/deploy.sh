d7b95fcbbe3b98086987f67c1322f5104c9842b4
Cloud User <sandeep@Docker.s0kw3boyjc4e5okf0htvp3mykg.dx.internal.cloudapp.net>
2020-08-25 11:51:45 +0000

    Added the private key of jenkin user in the ssh command

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -3,7 +3,7 @@ echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp /tmp/.auth RAID-VM-Test:/tmp/.auth
-scp ./jenkins/deploy/publish RAID-VM-Test:/tmp/publish
+scp -i /var/lib/jenkins/.ssh/id_rsa /tmp/.auth sandeep@RAID-VM-Test:/tmp/.auth
+scp -i /var/lib/jenkins/.ssh/id_rsa ./jenkins/deploy/publish sandeep@RAID-VM-Test:/tmp/publish
 
 ssh -i /var/lib/jenkins/.ssh/id_rsa sandeep@RAID-VM-Test "/tmp/publish"
